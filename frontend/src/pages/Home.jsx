11f2aa35cd628583a0d58cb49a0d5c8b80af84da
knicolasora <ka416333@ucf.edu>
2024-06-19 15:18:56 -0400

    web dev testing

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css'; // Adjust the path if necessary
+
+const Home = () => {
+    return (
+        <nav className="navbar">
+          <div className="navbar-title">
+            For the People, Find the People
+          </div>
+        </nav>
+      );
+    };
+export default Home;
