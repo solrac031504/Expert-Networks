a87fd071d36ee0f49d02c136cf5a52acab2217ac
Jovanny Vasquez <jovannyvasquez15@gmail.com>
2024-06-27 15:11:52 -0400

    Added support for fetchExperts

--- backend/routes/experts.js
+++ backend/routes/experts.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const {
+  /*
   getExperts, 
   getExpert, 
+  */
   createExpert, 
   deleteExpert, 
-  updateExpert
+  updateExpert,
+  fetchExperts
 } = require('../controllers/expertController')
 
 const router = express.Router()
-
+//Commented out for issues with fetchExperts
+/*
 // GET all experts
 router.get('/', getExperts)
 
 // GET a single expert
 router.get('/:id', getExpert)
-
+*/
 // POST a new expert
 router.post('/', createExpert)
 
@@ -23,5 +27,8 @@ router.delete('/:id', deleteExpert)
 
 // UPDATE a expert
 router.patch('/:id', updateExpert)
+
+//GET experts from API
+router.get('/fetch', fetchExperts)
 
 module.exports = router
