04ef42f8cc33230b097e5920859c3395a4047364
Jovanny Vasquez <jovannyvasquez15@gmail.com>
2024-06-27 15:10:10 -0400

    Updated model for author id

--- backend/models/expertModel.js
+++ backend/models/expertModel.js
@@ -3,42 +3,61 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const expertSchema = new Schema({
+  // Used for searching the profiles for more information
+  expert_id: {
+    type: String,
+    required: true,
+    default: 'N/A'
+  },
   name: {
     type: String,
-    required: true
+    required: true,
+    default: 'N/A'
   },
   field_of_study: {
     type: String,
-    required: true
+    required: true,
+    default: 'N/A'
   },
   institution: {
     type: String,
-    required: true
+    required: true,
+    default: 'N/A'
   },
   region: {
     type: String,
-    required: true
+    required: true,
+    default: 'N/A'
   },
   citations: {
     type: Number,
-    required: true
+    required: true,
+    default: 0
   },
   hindex: {
     type: Number,
-    required: true
+    required: true,
+    default: 0
+  },
+  i10_index: {
+    type: Number,
+    required: true,
+    default: 0
   },
   age: {
     type: Number,
-    required: false,
-    default: -1
+    required: true,
+    default: 0
   },
   years_in_field: {
     type: Number,
-    required: true
+    required: true,
+    default: 0
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    default: 'N/A'
   }
 }, { timestamps: true })
 
