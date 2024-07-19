d7f86020e1dc67f7966a50c747760e15801fc71e
solrac031504 <carlos.gonzalez031504@gmail.com>
2024-07-19 18:47:54 -0400

    Added default 000000 institution_id into Expert model

--- backend/models/Expert.js
+++ backend/models/Expert.js
@@ -24,6 +24,7 @@ const Expert = sequelize.define('Expert', {
       key: 'institution_id',
     },
     allowNull: false,
+    defaultValue: '000000'
   },
   citations: {
     type: DataTypes.INTEGER,
