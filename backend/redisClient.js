5cb892397c25b8eec838bb724d5e6fb476226a0a
solrac031504 <carlos.gonzalez031504@gmail.com>
2024-09-28 16:13:54 -0400

    Connected to DO Redis and new build folder

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -1,5 +1,10 @@
 const redis = require('redis');
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+  tls: {}  // Required for Managed Redis with SSL
+});
 
 redisClient.on('error', (err) => {
   console.log('Redis error: ', err);
