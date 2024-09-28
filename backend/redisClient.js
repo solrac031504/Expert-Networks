50fd7daa501122d5806db20be5c535397328c2c6
solrac031504 <carlos.gonzalez031504@gmail.com>
2024-09-28 16:54:19 -0400

    Fixing Redis connection

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -3,7 +3,7 @@ const redisClient = redis.createClient({
   username: process.env.REDIS_USER,
   password: process.env.REDIS_PSWD,
   host: process.env.REDIS_HOST,
-  port: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
   tls: true
 });
 
