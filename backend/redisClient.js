e39bccaa6fdedc46b7be47d78708e0e1edce54fb
solrac031504 <carlos.gonzalez031504@gmail.com>
2024-09-28 17:20:46 -0400

    Debugging deployment

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -17,4 +17,13 @@ redisClient.on('connect', () => {
   console.log('Connected to Redis');
 });
 
+// Log connection details (excluding sensitive information)
+console.log('Attempting to connect to Redis with:', {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PSWD,
+  tls: true
+});
+
 module.exports = redisClient;
