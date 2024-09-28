6e94ab744d76d182d5fb135e2bdcbc740bd5bdff
solrac031504 <carlos.gonzalez031504@gmail.com>
2024-09-28 17:03:58 -0400

    Updating redis connection

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -1,9 +1,10 @@
 const redis = require('redis');
 const redisClient = redis.createClient({
-  username: process.env.REDIS_USER,
-  password: process.env.REDIS_PSWD,
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PSWD,
+  database: process.env.REDIS_DB,
   tls: true
 });
 
