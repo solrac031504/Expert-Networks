4f389e8805c82ef43ce6f91bee0907afb5f7107f
solrac031504 <carlos.gonzalez031504@gmail.com>
2024-09-28 18:05:01 -0400

    Fixing redis connection?

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -1,12 +1,19 @@
 const redis = require('redis');
 require('dotenv').config();
 
+// const redisClient = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+//   username: process.env.REDIS_USER,
+//   password: process.env.REDIS_PSWD,
+//   tls: true
+// });
+
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  username: process.env.REDIS_USER,
-  password: process.env.REDIS_PSWD,
-  tls: true
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true
+  }
 });
 
 redisClient.on('error', (err) => {
@@ -15,15 +22,6 @@ redisClient.on('error', (err) => {
 
 redisClient.on('connect', () => {
   console.log('Connected to Redis');
-});
-
-// Log connection details (excluding sensitive information)
-console.log('Attempting to connect to Redis with:', {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  username: process.env.REDIS_USER,
-  password: process.env.REDIS_PSWD,
-  tls: true
 });
 
 module.exports = redisClient;
