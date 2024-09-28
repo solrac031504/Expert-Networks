2651239763fb180a99e40f965be728cbe2b380f1
solrac031504 <carlos.gonzalez031504@gmail.com>
2024-09-28 16:48:43 -0400

    Fixed Redis connection and new build

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -1,9 +1,10 @@
 const redis = require('redis');
 const redisClient = redis.createClient({
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PSWD,
   host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-  tls: {}  // Required for Managed Redis with SSL
+  port: process.env.REDIS_HOST,
+  tls: true
 });
 
 redisClient.on('error', (err) => {
