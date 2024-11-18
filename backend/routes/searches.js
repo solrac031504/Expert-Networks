a5e69c46e6e67489eddbbd49cfa58318a633398e
solrac031504 <carlos.gonzalez031504@gmail.com>
2024-11-18 16:45:09 -0500

    Removed keep alive request

--- backend/routes/searches.js
+++ backend/routes/searches.js
@@ -8,9 +8,4 @@ const router = express.Router();
 // GET search experts
 router.get('/', searchExperts);
 
-// Keep-alive route to prevent timeout
-router.get('/keep-alive', (req, res) => {
-  res.sendStatus(200);
-});
-
 module.exports = router
